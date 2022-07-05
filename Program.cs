27e51f27c6b14742c81e670755c7a47ceadc7fe2
edwink.ronoh@gmail.com <edwink.ronoh@gmail.com>
2022-07-05 18:15:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank_ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int[] reverse = Enumerable.Reverse(arr).ToArray();
+            Console.WriteLine(String.Join(' ', reverse));
+        }
+    }
+}
